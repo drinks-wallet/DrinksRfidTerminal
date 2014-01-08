80263f8fa142bd881583688689a46ab6f42c2336
Benoit Blanchon <benoit.blanchon@gmail.com>
2014-01-08 13:44:28 +0100

    Periodically query catalog to the server

--- DrinksRfidTerminal.ino
+++ DrinksRfidTerminal.ino
@@ -13,6 +13,7 @@
 #include "WebApi.h"
 
 #define CLOCK_SYNC_PERIOD  1*60*1000
+#define CATALOG_SYNC_PERIOD  1*60*1000
 
 static Buttons buttons;
 static Catalog catalog;
@@ -23,6 +24,7 @@ static RfidReader rfid;
 static WebApi api;
 
 static unsigned long clockSyncTime = 0;
+static unsigned long catalogSyncTime = 0;
 
 void setup() 
 {  
@@ -39,29 +41,15 @@ void setup()
   rfid.begin();
 
   display.setText(0, "Connecting...");    
-
-  Serial.println("CATALOG:");
-
-  api.getCatalog(catalog);
-
-  for( int i=0 ; i<catalog.getProductCount() ; i++ )
-  {
-    Serial.print("[");
-    Serial.print(i);
-    Serial.print("]");
-    Serial.println(catalog.getProduct(i));    
-  }
-
-  buttons.setCount(catalog.getProductCount());
-
-  display.setText(0, catalog.getHeader());
 }
 
 static void syncClockIfNeeded()
 {
   if( clockSyncTime != 0 && clockSyncTime + CLOCK_SYNC_PERIOD < millis() ) 
     return;
-  
+    
+  Serial.print("TIME = ");  
+    
   unsigned long time = api.getTime();
   
   if( time != 0 )
@@ -73,22 +61,50 @@ static void syncClockIfNeeded()
   {
     clockSyncTime = 0;
   }
-  
-   Serial.print("TIME = ");
+
   Serial.println(time); 
 }
 
+static void syncCatalogIfNeeded()
+{ 
+  if( catalogSyncTime != 0 && catalogSyncTime + CATALOG_SYNC_PERIOD < millis() ) 
+    return;
+    
+   Serial.println("CATALOG:");
 
+  api.getCatalog(catalog);
+
+  for( int i=0 ; i<catalog.getProductCount() ; i++ )
+  {
+    Serial.print("[");
+    Serial.print(i);
+    Serial.print("]");
+    Serial.println(catalog.getProduct(i));    
+  }
+  
+  if( catalog.getProductCount() > 0 )
+  {      
+    catalogSyncTime = millis(); 
+  }
+  else
+  {
+    catalogSyncTime = 0;
+  }
+}
 
 void loop() 
 {  
   syncClockIfNeeded();
+  syncCatalogIfNeeded();
   
-  if( clockSyncTime == 0 )
+  if( clockSyncTime == 0 || catalogSyncTime == 0)
   {
    display.setText(0, "* * * ERROR * * *"); 
    display.setText(1, "Connection failed");
   }
+  
+  buttons.setCount(catalog.getProductCount());
+  display.setText(0, catalog.getHeader());
   
   int product = buttons.getSelectedIndex();
 
