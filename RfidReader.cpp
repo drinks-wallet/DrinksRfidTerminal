3a8630cef5e17237eba647ebcfd87ed415fe4e6c
Benoit Blanchon <benoit.blanchon@gmail.com>
2014-01-28 13:41:25 +0100

    Cleaned RfidReader

--- RfidReader.cpp
+++ RfidReader.cpp
@@ -7,32 +7,22 @@
 */
 
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
 #include "RfidReader.h"
-#include "Pins.h"
 
-static SoftwareSerial RFID(PIN_RFID, PIN_RFID); // RX and TX
-static char buffer[16];
-
-void RfidReader::begin()
-{
-	RFID.begin(9600);    // start serial to RFID reader
-}
-
-static char parseHexNibble(char c)
+byte RfidReader::parseHexNibble(char c)
 {
 	if (c >= 'A') return c - 'A' + 0xA;
 	if (c >= 'a') return c - 'a' + 0xA;
 	return c - '0';
 }
 
-static char parseHexByte(char* str)
+byte RfidReader::parseHexByte(char* str)
 {
 	return (parseHexNibble(str[0]) << 4) | parseHexNibble(str[1]);
 }
 
-static char computeCheckSum(char badge [])
+byte RfidReader::computeCheckSum(char badge[])
 {
 	char checkSum = 0;
 
@@ -44,29 +34,28 @@ static char computeCheckSum(char badge [])
 	return checkSum;
 }
 
-
 char* RfidReader::tryRead()
 {
 	int i;
 
-	if (RFID.available() < 14) return NULL;
+	if (rfid.available() < 14) return NULL;
 
 	for (i = 0; i < 14; i++)
-		buffer[i] = RFID.read();
+		buffer[i] = rfid.read();
 
 	if (buffer[0] != 2 || buffer[13] != 3)
 	{
-		RFID.flush();
-		Serial.println("UNSYNCED");
+		rfid.flush();
+		Serial.println("RFID unsynced !");
 		return NULL;
 	}
 
-	char readCheckSum = parseHexByte(buffer + 11);
-	char computedCheckSum = computeCheckSum(buffer + 1);
+	byte readCheckSum = parseHexByte(buffer + 11);
+	byte computedCheckSum = computeCheckSum(buffer + 1);
 
 	if (readCheckSum != computedCheckSum)
 	{
-		Serial.println("INVALID CHECK SUM");
+		Serial.println("RFID checksum !");
 		return NULL;
 	}
 
--- RfidReader.h
+++ RfidReader.h
@@ -9,12 +9,33 @@
 #ifndef _RFIDREADER_H
 #define _RFIDREADER_H
 
+#include <SoftwareSerial.h>
+
+#include "Pins.h"
+
 class RfidReader
 {
 public:
 
-	void begin();
+	RfidReader() : rfid(PIN_RFID, PIN_RFID)
+	{
+	}
+
+	void begin()
+	{
+		rfid.begin(9600);
+	}
+
 	char* tryRead();
+
+private:
+
+	byte parseHexNibble(char);
+	byte parseHexByte(char*);
+	byte computeCheckSum(char*);
+
+	SoftwareSerial rfid;
+	char buffer[14];
 };
 
 #endif
