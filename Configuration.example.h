12dff45e832574993df833f8ea507a3d56e29d9b
Benoit Blanchon <benoit.blanchon@gmail.com>
2014-02-05 13:03:45 +0100

    Added Configuration.example.h, added Configuration.h in the ignore list

--- Configuration.example.h
+++ Configuration.example.h
@@ -9,12 +9,17 @@
 #ifndef _CONFIGURATION_H
 #define _CONFIGURATION_H
 
+//  PLEASE READ ! ! !
+// 
+//  You need to rename this file into "Configuration.h" in order to compile the project.
+//  Then you must change the values to match you actual configuration.
+
 #define PRIVATE_KEY {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f}
 
-#define MAC_ADDRESS {0x00,0xAA,0xBB,0xCC,0xDE,0x02 }
+#define MAC_ADDRESS {0x01,0x23,0x45,0x67,0x89,0xAB}
 
-#define SERVER_NAME "lbotelho-pc.satimo2000.local"
+#define SERVER_NAME "drinks.example.com"
 
-#define SERVER_PORT 8080
+#define SERVER_PORT 80
 
 #endif
